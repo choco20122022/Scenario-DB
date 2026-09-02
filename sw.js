be0037138a8d1009643692fc6d522ab64401dd97
choco20122022 <inches-cube.8o@icloud.com>
2026-09-02 16:16:00 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "scenario-db-v2";
+const CACHE_NAME = "scenario-db-v4";
 const ASSETS = ["./", "./index.html", "./manifest.json", "./icon.svg", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e)=>{
